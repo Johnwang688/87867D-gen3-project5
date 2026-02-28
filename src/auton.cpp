b7b0b62c079ccf7b7cbf6343b4dee0849405f05c
John Wang <john.wang.09.18.09@gmail.com>
2026-02-28 07:42:36 -0600

    update skills dsr to match lighting conditions at state

--- src/auton.cpp
+++ src/auton.cpp
@@ -420,7 +420,7 @@ namespace bot {
             double heading_correct;
             if (left_distance == 9999 || left_distance <= 0) heading_correct = 0.0;
             else if (left_distance < 300) heading_correct = (400 - left_distance) * 0.07;
-            else heading_correct = (900 - left_distance) * 0.07;
+            else heading_correct = (850 - left_distance) * 0.07;
             vex::task::sleep(50);
             dt.drive(650, 1500, 60, 100+heading_correct);
             dt.drive_for(450 - (10.0*heading_correct), 1000, 50, 100+heading_correct);
@@ -456,7 +456,7 @@ namespace bot {
             double right_distance = bot::sensors::right_dist.objectDistance(vex::mm);
             double heading_correct_2;
             if (right_distance == 9999 || right_distance <= 0) heading_correct_2 = 0.0;
-            else heading_correct_2 = (650 - right_distance);
+            else heading_correct_2 = (700 - right_distance);
             dt.drive(-250 - sqrt(2.0)*heading_correct_2, 1500, 40, -45);
             dt.drive(-350, 1000, 40, -45);
             bot::pistons::match_load_piston.set(false);
@@ -471,7 +471,7 @@ namespace bot {
                 dt.turn_to_heading(-45, 1000, 100);
                 return 0;
             });
-            vex::task::sleep(1200);
+            vex::task::sleep(1500);
             bot::motors::lower.stop();
             vex::task::sleep(300);
             bot::motors::mid.stop();
@@ -483,7 +483,7 @@ namespace bot {
             double dsr_distance;
             if (left_distance_2 == 9999 || left_distance_2 <= 0) dsr_distance = 350.0;
             else if (left_distance_2 < 700.0) dsr_distance = left_distance_2 - 200.0;
-            else dsr_distance = left_distance_2 - 700.0;
+            else dsr_distance = left_distance_2 - 720.0;
             bot::pistons::match_load_piston.set(true);
             dt.drive(400, 1500, 50, 20);
             dt.drive(dsr_distance, 1200, 60, 20);
